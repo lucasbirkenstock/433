9c2aa16c94783d42172d1356f76a245f4f719ad1
lucasbirkenstock <root@DESKTOP-CDB77Q6>
2024-04-16 00:28:27 -0700

    Progress

--- assign4/main.cpp
+++ assign4/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 4: Producer Consumer Problem
  * @file main.cpp
- * @author ??? (TODO: your name)
+ * @author Lucas Birkenstock, Connor Toro
  * @brief The main program for the producer consumer problem.
  * @version 0.1
  */
@@ -20,7 +20,6 @@ Buffer buffer;
 pthread_mutex_t mutex;
 
 // Producer thread function
-// TODO: Add your implementation of the producer thread here
 void *producer(void *param) {
     // Each producer insert its own ID into the buffer
     // For example, thread 1 will insert 1, thread 2 will insert 2, and so on.
@@ -48,7 +47,6 @@ void *producer(void *param) {
 }
 
 // Consumer thread function
-// TODO: Add your implementation of the consumer thread here
 void *consumer(void *param) {
     buffer_item item;
 
@@ -67,26 +65,55 @@ void *consumer(void *param) {
             cout << "Consumer error condition" << endl;    // shouldn't come here
         }
     }
+
     // Unlock the thread
     pthread_mutex_unlock(&mutex);
 }
 
 int main(int argc, char *argv[]) {
-    /* TODO: 1. Get command line arguments argv[1],argv[2],argv[3] */
+    /* 1. Get command line arguments argv[1],argv[2],argv[3] */
     if (argc == 4) {
-        //------------------------------- TODO may need to convert these to int --------------------------
-        char* sleep_time = argv[1]; // How long main thread sleeps before terminating (seconds)
-        char* num_producers = argv[2]; 
-        char* num_consumers = argv[3]; 
+        int sleep_time = std::stoi(argv[1]);    // How long main thread sleeps before terminating (seconds)
+        int num_producers = std::stoi(argv[2]); // Convert input to int
+        int num_consumers = std::stoi(argv[3]); // Convert input to int 
+
+        /* TODO: 2. Initialize buffer and synchronization primitives */
+        Buffer theBuffer = Buffer(5);
+        pthread_mutex_init(&mutex, NULL);
+
+        /* TODO: 3. Create producer thread(s).
+        * You should pass an unique int ID to each producer thread, starting from 1 to number of threads */
+
+        // Create an array of pthread identifiers to identify the producer threads which will be created
+        pthread_t producer_threads[num_producers];
+
+        for (int i = 1; i <= num_producers; ++i) {
+
+        }
+
+
+        /* TODO: 4. Create consumer thread(s) */
+        // Create an array of pthread identifiers to identify the consumer threads which will be created
+        pthread_t consumer_threads[num_consumers];
+
+        for (int i = 1; i <= num_consumers; ++i) {
+            // Create a consumer thread
+            // Access index i-1 of the identifier array (because this loop starts at 1)
+            // NULL specifies default thread attributes
+            // Point to consumer function
+            // Idk the last one
+            pthread_create(&consumer_threads[i - 1], NULL, consumer, (void *)&i);
+        }
+        /* 5. Main thread sleep */
+        sleep(sleep_time);
+
+
     } else {
         // Print error if there is an incorrect number of inputs
         std::cerr << "Usage" << argv[0] << " <arg1> <arg2> <arg3>" << std::endl;
     }
-    /* TODO: 2. Initialize buffer and synchronization primitives */
-    Buffer theBuffer = Buffer(5);
-    /* TODO: 3. Create producer thread(s).
-     * You should pass an unique int ID to each producer thread, starting from 1 to number of threads */
-    /* TODO: 4. Create consumer thread(s) */
-    /* TODO: 5. Main thread sleep */
+    
+
     /* TODO: 6. Exit */
+    exit(0);
 }
